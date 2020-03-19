06a93a2745820577c51b7a0f171e7eb1468cdee3
Julian Akani <julian.akani@hotmail.fr>
2020-03-19 12:33:19 +0100

    Modification simulateur de demande

--- Client.py
+++ Client.py
@@ -12,20 +12,19 @@ class Client:
     def __init__(self,n):
         """3 attributs à la classe Client : 
             alpha la sensibilité au prix
-            day le jour d'arrivée 
+            time_range l'intervalle de temps où le client arrive
             erreur qui est le bruit de la fonction utilité, dépend uniquement 
-            du temps et mais pas du client et du vol """
-        self.day = n                 #jour d'arrivée du client, qui est unique pour chaque client et chaque client arrive un jour (bijection)
-        self.alpha = alpha(self.day) #valeur prise arbitrairement, la sensibilité décroit avec le temps
-        self.erreur = 0              #le bruit du client
+            de time_range et mais pas du client et du vol """
+        self.time_range = n                 #intervalle de temps où le client arrive. Deux clients différents ont un intervalle de temps différent (injection)
+        self.alpha = alpha(self.time_range) #valeur prise arbitrairement, la sensibilité décroit avec le temps
+        self.erreur = 0                     #le bruit du client
     
     
     def gumbel(self):
         """Donne à l'erreur du client une valeur aléatoire selon une loi de 
         Gumbel de paramètre 0 et 1
         0 est le paramètre de position
-        10-self.day est le paramètre d'étalement
-        l'erreur dépend uniquement du temps"""
+        1 est le paramètre d'étalement"""
         self.erreur = numpy.random.gumbel(0, 1)
 
 
