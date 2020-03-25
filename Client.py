93d06181e6b3ae263ba63a3dc8f9fded0653e298
Unknown <fbarras78@gmail.com>
2020-03-25 11:18:23 +0100

    Delete rapport + Modif main et client

--- Client.py
+++ Client.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 import numpy
+import math
 
-a0 = 1
-k = 0.1
-alpha = lambda d : a0 - k*d
+#Sensibilité au prix selon le temps t (jour)
+#Modèle linéaire décroissant variant entre t0=0 et tf
+#tf est le nombre de jours d'ouverture de la billetterie, 30 jours par ex
+
+def alpha(t):
+    alpha_0=10
+    alpha_f=1
+    tf=30
+    return alpha_0 + t/tf * (alpha_f - alpha_0)
+
+#Utilité relative au prix p selon le temps t (jour)
+def price_sensitivity(p,t):
+    return math.exp(-alpha(t)*p)
 
 class Client:
     def __init__(self,n):
@@ -17,15 +27,6 @@ class Client:
             de time_range et mais pas du client et du vol """
         self.time_range = n                 #intervalle de temps où le client arrive. Deux clients différents ont un intervalle de temps différent (injection)
         self.alpha = alpha(self.time_range) #valeur prise arbitrairement, la sensibilité décroit avec le temps
-        self.erreur = 0                     #le bruit du client
-    
-    
-    def gumbel(self):
-        """Donne à l'erreur du client une valeur aléatoire selon une loi de 
-        Gumbel de paramètre 0 et 1
-        0 est le paramètre de position
-        1 est le paramètre d'étalement"""
-        self.erreur = numpy.random.gumbel(0, 1)
 
 
     
