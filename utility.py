0da685086f200c6598613137819ed0b629951a31
Unknown <fbarras78@gmail.com>
2020-03-25 13:10:46 +0100

    Ajout de la fonction de choix

--- utility.py
+++ utility.py
@@ -1,7 +1,7 @@
 ### MODELES DE PREFERENCES
 
-from Vol import *
-from Client import *
+import numpy as np
+import math
 
 #Modèle de sensibilité au prix selon le temps t (jour)
 #Modèle linéaire décroissant variant entre t0=0 et tf
@@ -19,10 +19,24 @@ def price_utility(C,V):
 #Fonction utilité du client relative à l'horaire h du vol (chameau)
 #Fonction entre 0 et 1 ?
 def time_utility(C,V):
-    res = h : (5 + math.cos(h-8) - (h-8)*math.cos(h-8))/12.3507
+    res = lambda h : (5 + math.cos(h-8) - (h-8)*math.cos(h-8))/12.3507
     return res(V.time_departure)
 
-#Partie déterministe Ui(xj) de l'utilité du vol V pour le client C
+#Partie déterministe vi(xj) de l'utilité du vol V pour le client C
 def utility(C,V):
     theta = 1
     return theta * price_utility(C,V) + time_utility(C,V)
+
+#Selon une liste de vols Vi flights de longueur n, choix du client Cd'un vol ou de 0
+#On définit la variable de choix yi par la PMF
+#v0 est l'utilité du choix 0
+def choice(C,flights,v0):
+    n = len(flights)
+    choices = [k for k in range(n+1)]
+    utilities = [math.exp(v0)]
+    for flight in flights:
+        utilities.append(math.exp(utility(C,flight)))
+    s = sum(utilities)
+    probabilities = [u/s for u in utilities]
+    return np.random.choice(choices, 1, p=probabilities)
+
