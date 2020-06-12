6c47c5823f1218d06a0ccc0992055a681c08aaba
François <fbarras78@gmail.com>
2020-06-12 16:57:48 +0200

    Wandb integration

--- spinup_ddpg.py
+++ spinup_ddpg.py
@@ -1,25 +1,28 @@
 import tensorflow as tf
 import spinup
 
+import wandb
+
 #Importer les autre scripts
 from gym_env import *
 from vol import *
 
+# horizon temporel (longueur d'un épisode = nombre de clients si demande equirepartie)
+T = 9
+
 # Initialisation des vols
 
 h1 = Horaire(14,0)
-h2 = Horaire(19,0)
+V1 = Vol(h1,T+1)
 
-V1 = Vol(h1,10)
+h2 = Horaire(19,0)
 V2 = Vol(h2,10)
 
 flights = [V1]
-
-# horizon temporel (longueur d'un épisode = nombre de clients si demande equirepartie)
-T = 1
-
 # Range d'exploration du prix
 max_price = 200
+
+name = "ddpg_" + str(T) + "_V1"
 
 # On construit l'environnement Gym
 env = RMenv(flights, T, max_price, verbose = 0)
@@ -30,46 +33,82 @@ env_fn = lambda : env
 from stable_baselines.common.env_checker import check_env
 check_env(env, warn=True)
 
+hp = dict(
+
+    #env params
+    max_price = max_price,
+    time_horizon = T,
+
+    # epochs
+    epochs=100,
+    # steps = interactions = clients
+    steps_per_epoch=4000,
+    # Number of new env interactions btw each new grad descent
+    update_every=100, 
+
+    #Uniform start steps
+    start_steps=8000, 
+    # Min env interactions before first grad descent
+    update_after=1000,
+
+    # Size of replay buffer
+    replay_size=100000, 
+    # Batch size from buffer for grad descent
+    batch_size=1000, 
+
+
+    #Learning rates
+    pi_lr = 0.001, 
+    q_lr = 0.001, 
+    # Bruit gaussien random de std dev = act_noise
+    act_noise=0.5,
+    # Discount = 1 for finite horizon
+    gamma=0.99, 
+    # "Lag" of the target network
+    polyak=0.1, 
+
+    # Num test episodes ??
+    num_test_episodes=1000,
+
+    # Episode length (si done == false tout le temps)
+    # en pratique doit etre supérieur à min(total seats, total clients)
+    max_ep_len=1000, 
+
+    logger_kwargs={}, 
+    save_freq=1
+    #actor_critic=<function mlp_actor_critic>
+    #ac_kwargs={}, 
+    #seed=0
+
+    )
+
+
+
+wandb.init(project=name, sync_tensorboard=True,config=hp)
+config = wandb.config
+
+
 # On run l'algo
 spinup.ddpg_tf1(env_fn, 
 
-""" Hyperparamètres à tuner"""
-# epochs
-epochs=100,
-steps_per_epoch=4000,
-
-# Num test episodes ??
-num_test_episodes=20,
-
-# Minibatch
-batch_size=100, 
-
-#Learning rates
-pi_lr=0.001, 
-q_lr=0.001, 
-
-# Bruit gaussien random de std dev = act_noise
-act_noise=0.1, 
-#Discount = 1 for finite horizon
-gamma=0.99, 
-#Uniform start steps
-start_steps=1000, 
-
-
-replay_size=1000000, 
-polyak=0.995, 
-
-#Update 
-update_after=1000, 
-update_every=50, 
-# Episode length (si done == false tout le temps)
-# en pratique doit etre supérieur à min(total seats, total clients)
-max_ep_len=1000, 
-
-logger_kwargs={}, 
-save_freq=1
+epochs = config.epochs,
+steps_per_epoch = config.steps_per_epoch,
+start_steps = config.start_steps, 
+update_after = config.update_after,
+update_every = config.update_every, 
+replay_size = config.replay_size, 
+batch_size = config.batch_size, 
+pi_lr = config.pi_lr, 
+q_lr = config.q_lr, 
+act_noise = config.act_noise,
+gamma = config.gamma, 
+polyak = config.polyak, 
+num_test_episodes = config.num_test_episodes,
+max_ep_len = config.max_ep_len, 
+#logger_kwargs={}, 
+#save_freq=1
 #actor_critic=<function mlp_actor_critic>
 #ac_kwargs={}, 
-#seed=0,
+#seed=0
 
 )
